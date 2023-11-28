f3f270651446af2d0374a409525d101fac7cba92
fx900-fx97@7 <ss365493@gmail.com>
2023-11-28 21:28:24 +0530

    Production ready folder structure for backend

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import { asyncHandler } from "./../utils/asyncHandler.js";
+import { ApiError } from "./../utils/ApiError.js";
+import { User } from "./../models/user.model.js";
+import { uploadOnCloudinary } from "./../utils/cloudinary.service.js";
+import { ApiResponse } from "./../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, userName, password } = req.body;
+  if (
+    [fullName, email, userName, password].some((field) => {
+      field?.trim() === "";
+    })
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = await User.findOne({
+    $or: [{ userName }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverLocalPath = req.files?.coverImage[0]?.path;
+  let coverLocalPath ;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverLocalPath = req.files.coverImage[0].path;
+  }
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    userName: userName.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User created successfully"));
+});
+
+export { registerUser };
